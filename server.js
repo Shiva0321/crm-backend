3eafb12823b20cde1126888c8d31e5e2a189f074
Shiva Gupta <shivascdgupta@gmail.com>
2024-12-02 01:38:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api', orderRoutes); // Order-related routes
 
 
 // Connect to MongoDB using Mongoose
-mongoose.connect('mongodb://localhost:27017/crmgpt', {
+mongoose.connect('mongodb://localhost:27017/crm-db', {
   useNewUrlParser: true, // Use the new MongoDB connection string parser
   useUnifiedTopology: true, // Use the new topology engine
 }).then(() => {
